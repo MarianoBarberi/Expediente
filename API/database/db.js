dcb4f9f1acbe48ffbd9380b4289bbaebfc4e7937
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-05-24 13:41:21 -0600

    connection

--- API/database/db.js
+++ API/database/db.js
@@ -1,24 +1,32 @@
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 const dbConfig = require("../config/db.config.js");
 
-// Create a connection to the database
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    user: process.env.DB_USER,
+// Create a connection pool
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER,
 
-    // host: dbConfig.HOST,
-    // user: dbConfig.USER,
-    // password: dbConfig.PASSWORD,
-    // database: dbConfig.DB
-  });
-
-// open the MySQL connection
-connection.connect(error => {
-    if (error) throw error;
-    console.log("Successfully connected to the database.");
+  // host: dbConfig.HOST,
+  // user: dbConfig.USER,
+  // password: dbConfig.PASSWORD,
+  // database: dbConfig.DB
 });
 
+// Get a connection from the pool
+pool.getConnection((error, connection) => {
+  if (error) throw error;
 
-module.exports = connection;
+  // Execute your command here
+  connection.query('SELECT * FROM cars', (error, results) => {
+    if (error) throw error;
+
+    // Process the results
+
+    // Release the connection back to the pool
+    connection.release();
+  });
+});
+
+module.exports = pool;
