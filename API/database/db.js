f5991235c88296ff853c291844b81605ea6a004e
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-05-24 12:49:03 -0600

    db

--- API/database/db.js
+++ API/database/db.js
@@ -1,41 +1,19 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2')
 const dbConfig = require("../config/db.config.js");
 
-async function getConnection() {
-    const connection = await mysql.createConnection({
-        host: dbConfig.HOST,
-        user: dbConfig.USER,
-        password: dbConfig.PASSWORD,
-        database: dbConfig.DB
-    });
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB
+  });
 
-    // Check if connection is established
-    try {
-        await connection.connect();
-        console.log("Successfully connected to the database.");
-    } catch (error) {
-        console.error("Database connection failed:", error);
-        throw error;
-    }
+// open the MySQL connection
+connection.connect(error => {
+    if (error) throw error;
+    console.log("Successfully connected to the database.");
+});
 
-    return connection;
-}
 
-async function executeQuery(query, params) {
-    const connection = await getConnection();
-
-    try {
-        const [results, ] = await connection.execute(query, params);
-        return results;
-    } catch (error) {
-        console.error("Query execution failed:", error);
-        throw error;
-    } finally {
-        await connection.end();
-    }
-}
-
-module.exports = {
-    getConnection,
-    executeQuery
-};
+module.exports = connection;
